19495d949fda7968c6422e8672f1de64dd6a4bca
beamaeb79 <s6135512010@phuket.psu.ac.th>
2020-09-15 07:44:13 +0700

    Checkpoint4

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,14 +1,35 @@
-import React, { useState } from 'react';
-export default function CharacterCard(props) {
-    const [active, setActive] = useState(false);
-    const activate = () => {
-        if(!active){
-            setActive(true)
-            props.activationHandler(props.value)
+import React, { Component } from 'react';
+import './App.css';
+export default class CharacterCard extends Component {
+    
+    constructor(props) {
+        super(props)
+        this.state = {
+            active: false,
         }
     }
-    const className = `card ${active ? 'activeCard': ''}`
+    
+
+    activate = () => {
+        if (!this.state.active) {
+            this.props.activationHandler(this.props.value)
+            this.setState({ active: true })
+        }
+    }
+
+    render() {
+        let className = `card ${this.state.active ? 'activeCard' : ''}`
         return (
-            <div className={className} onClick={activate}>{props.value}</div>
-        )      
+            <div className={className} onClick={this.activate}>
+                {this.props.value}
+            </div>
+        )
+    }
+
+
+    componentDidUpdate(prevProps){
+        if(prevProps.attempt !== this.props.attempt){
+          this.setState({active: false})
+          }  
+         }
 }
