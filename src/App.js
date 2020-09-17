e40e29b40cefdeaa5b0b644b0d8450771cf4409b
beamaeb79 <s6135512010@phuket.psu.ac.th>
2020-09-17 22:24:20 +0700

    commit2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App App-header">
-      <WordCard value="REACT EASY"/>
+      <WordCard value="SEVENTEEN"/>
     </div>
   );  
 }
